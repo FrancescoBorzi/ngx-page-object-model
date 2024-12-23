9194bcd1613681d22ac6cb04996cd3b7edbdf526
FrancescoBorzi <borzifrancesco@gmail.com>
2024-12-23 22:58:17 +0100

    docs: create documentation website

---
+++ website/docusaurus.config.ts
@@ -0,0 +1,87 @@
+import {themes as prismThemes} from 'prism-react-renderer';
+import type {Config} from '@docusaurus/types';
+import type * as Preset from '@docusaurus/preset-classic';
+
+// This runs in Node.js - Don't use client-side code here (browser APIs, JSX...)
+
+const config: Config = {
+  title: 'ngx-page-object-model',
+  tagline: 'Angular Component Tests made easy',
+  favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://francescoborzi.github.io/',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/ngx-page-object-model/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
+  organizationName: 'FrancescoBorzi', // Usually your GitHub org/user name.
+  projectName: 'ngx-page-object-model', // Usually your repo name.
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internationalization, you can use this field to set
+  // useful metadata like html lang. For example, if your site is Chinese, you
+  // may want to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
+  },
+
+  presets: [
+    [
+      'classic',
+      {
+        docs: {
+          sidebarPath: './sidebars.ts',
+          // Please change this to your repo.
+          // Remove this to remove the "edit this page" links.
+          editUrl:
+            'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+        },
+        theme: {
+          customCss: './src/css/custom.css',
+        },
+      } satisfies Preset.Options,
+    ],
+  ],
+
+  themeConfig: {
+    // Replace with your project's social card
+    image: 'img/docusaurus-social-card.jpg',
+    navbar: {
+      title: 'ngx-page-object-model',
+      logo: {
+        alt: 'ngx-page-object-model',
+        src: 'img/angular_icon_gradient.gif',
+      },
+      items: [
+        {
+          type: 'docSidebar',
+          sidebarId: 'tutorialSidebar',
+          position: 'left',
+          label: 'Documentation',
+        },
+        {
+          href: 'https://github.com/FrancescoBorzi/ngx-page-object-model',
+          label: 'GitHub',
+          position: 'right',
+        },
+      ],
+    },
+    footer: {
+      style: 'dark',
+      links: [],
+      copyright: `The <a href="https://github.com/FrancescoBorzi/ngx-page-object-model">ngx-page-object-model</a> library is open-source software released by <a href="https://nl.linkedin.com/in/francesco-borzi">Francesco Borzì</a> under the <a href="https://github.com/FrancescoBorzi/ngx-page-object-model/blob/main/LICENSE">MIT license</a>.`,
+    },
+    prism: {
+      theme: prismThemes.github,
+      darkTheme: prismThemes.dracula,
+    },
+  } satisfies Preset.ThemeConfig,
+};
+
+export default config;
